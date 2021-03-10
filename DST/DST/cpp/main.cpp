671b6f03acaa7f892eccb05075e12f7dc04a0645
xuekt <xuekt98@163.com>
2021-03-10 10:24:47 +0800

    add Stack and Queue

--- DST/DST/cpp/main.cpp
+++ DST/DST/cpp/main.cpp
@@ -3,7 +3,6 @@
 #include <cstdlib>
 #include <iostream>
 #include <ctime>
-#include <vector>
 
 template <typename T>
 struct increase {
@@ -12,17 +11,19 @@ struct increase {
 	}
 };
 
-int main() {
-	using std::cout;
-	using std::endl;
-
+void testDSTVector() {
 	DST::Vector<int> v;
 	for(int i = 0; i < 50; ++i) {
 		v.insert(rand() % 50);
 	}
+}
 
-	clock_t start = clock(), finish;
-	v.tranverse(increase<int>());
+int main() {
+	using std::cout;
+	using std::endl;
+
+	clock_t start, finish;
+	start = clock();
 	finish = clock();
 	cout << finish - start << endl;
 	system("pause");
